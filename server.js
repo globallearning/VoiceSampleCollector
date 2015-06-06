8188abac37c0e4f437c6af973e1c110b43306a4b
patrickms <github@morris-suzuki.com>
2015-06-06 14:23:27 -0400

    Update server.js

--- server.js
+++ server.js
@@ -76,8 +76,7 @@ app.get('/auth/google/callback',
 
 app.use('/', passport.authenticate('google', {  
 	scope: 'openid profile email',
-                                    failureRedirect: 'http://www.geek.com',
-                                    successRedirect: '/'}), function(req,res,done){
+                                    failureRedirect: 'http://www.geek.com'}), function(req,res,done){
 						 if(req.user){
 						 	console.log('USER INFO:', req.user);
 						 	done(null, req.user);
